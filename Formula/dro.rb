34982deb23427af32baec3b5b2c74e04abea88e4
anthonwellsjo <anthonwellsjo@gmail.com>
2022-12-12 18:00:15 +0000

    dro v0.1.13

--- Formula/dro.rb
+++ Formula/dro.rb
@@ -4,9 +4,9 @@
 class Dro < Formula
   desc "Super minimal CLI todo (dro) application."
   homepage "https://crates.io/crates/dro"
-  url "https://github.com/anthonwellsjo/dro/releases/download/v0.1.12/dro-mac.tar.gz"
-  version "v0.1.12"
-  sha256 "39488a2aac807dd957999819daace0068e184ddf47f70c0dd4916ae9c9c470e9"
+  url "https://github.com/anthonwellsjo/dro/releases/download/v0.1.13/dro-mac.tar.gz"
+  version "v0.1.13"
+  sha256 "fe18c4b8d20bc965fdd2e9a13176a3772d3c62ac2bb95292e28a73b9c05b3a87"
   license "MIT"
 
   depends_on "rust" => :build
