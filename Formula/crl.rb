d8895a96ebd53567c0df996b8d1b91be3c59f1e1
anthonwellsjo <anthonwellsjo@gmail.com>
2023-01-12 17:50:31 +0100

    feat: adding tap for crl

--- Formula/crl.rb
+++ Formula/crl.rb
@@ -1,10 +1,10 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 # PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-class Dro < Formula
-  desc "super minimal CLI todo (dro) application."
-  homepage "https://crates.io/crates/dro"
-  url "https://github.com/anthonwellsjo/dro/releases/download/v0.2.5/dro-v0.2.5-x86_64-apple-darwin.tar.gz"
+class Crl < Formula
+  desc "super minimal CLI clipboard manager."
+  homepage "https://crates.io/crates/crl"
+  url "https://github.com/anthonwellsjo/dro/releases/download/v0.2.5/crl-v0.2.5-x86_64-apple-darwin.tar.gz"
   version "v0.2.5"
   sha256 "f907f9b43122fd9cf01ded816ace4691ab8d8c91b18952c08de23569ba0f3f9e"
   license "MIT"
@@ -13,6 +13,6 @@ class Dro < Formula
 
   def install
     # ENV.deparallelize  # if your formula fails when building in parallel
-    bin.install "dro"
+    bin.install "crl"
   end
 end
