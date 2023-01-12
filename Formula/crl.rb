e6883221bab27a3d2a202837550909cdd553d2ae
anthonwellsjo <anthonwellsjo@gmail.com>
2023-01-12 18:04:27 +0100

    feat: force downgrading

--- Formula/crl.rb
+++ Formula/crl.rb
@@ -4,8 +4,8 @@
 class Crl < Formula
   desc "super minimal CLI clipboard manager."
   homepage "https://crates.io/crates/crl"
-  url "https://github.com/anthonwellsjo/dro/releases/download/v0.2.5/crl-v0.2.5-x86_64-apple-darwin.tar.gz"
-  version "v0.2.5"
+  url "https://github.com/anthonwellsjo/crl/releases/download/v0.1.0/crl-v0.2.5-x86_64-apple-darwin.tar.gz"
+  version "v0.1.0"
   sha256 "f907f9b43122fd9cf01ded816ace4691ab8d8c91b18952c08de23569ba0f3f9e"
   license "MIT"
 
