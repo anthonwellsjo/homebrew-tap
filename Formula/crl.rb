06f91cafa26e23ce581dcb63485012f3f7617f5c
anthonwellsjo <anthonwellsjo@gmail.com>
2023-01-12 20:54:05 +0000

    crl v0.1.4

--- Formula/crl.rb
+++ Formula/crl.rb
@@ -4,9 +4,9 @@
 class Crl < Formula
   desc "super minimal CLI clipboard manager."
   homepage "https://crates.io/crates/crl"
-  url "https://github.com/anthonwellsjo/crl/releases/download/v0.1.3/crl-v0.1.3-x86_64-apple-darwin.tar.gz"
-  version "v0.1.3"
-  sha256 "12ff72dc33ff9cfe4eebe5f52550287613b977826c9df7e382c9aa9a44fd6598"
+  url "https://github.com/anthonwellsjo/crl/releases/download/v0.1.4/crl-v0.1.4-x86_64-apple-darwin.tar.gz"
+  version "v0.1.4"
+  sha256 "09ecf7735496c1cbb002c47f62799d05b933ff1106c96da34638bbb4f8f19dac"
   license "MIT"
 
   depends_on "rust" => :build
