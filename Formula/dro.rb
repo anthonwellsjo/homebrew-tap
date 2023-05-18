9a0edbe66fdebb42a8fea9db7d93837e8a042ae1
Carl Anthon Wellsjö <58119759+anthonwellsjo@users.noreply.github.com>
2023-05-18 21:51:06 +0200

    dro v0.2.12 (#34)

--- Formula/dro.rb
+++ Formula/dro.rb
@@ -4,9 +4,9 @@
 class Dro < Formula
   desc "super minimal CLI todo (dro) application."
   homepage "https://crates.io/crates/dro"
-  url "https://github.com/anthonwellsjo/dro/releases/download/v0.2.11/dro-v0.2.11-x86_64-apple-darwin.tar.gz"
-  version "v0.2.11"
-  sha256 "7209458410bb4754789b9f1e0308a7dcc664071f8471e39c1f33f0329079282d"
+  url "https://github.com/anthonwellsjo/dro/releases/download/v0.2.12/dro-v0.2.12-x86_64-apple-darwin.tar.gz"
+  version "v0.2.12"
+  sha256 "eb4baef3f0dbccdc1545ac347765d1ea22241ef5912f1608b1d4f3f29047fe61"
   license "MIT"
 
   depends_on "rust" => :build
